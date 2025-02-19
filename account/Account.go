6098dbdbd081a233214ca5c4916de616510475f4
TheLionSin <crazy_boy_122@mail.ru>
2025-02-20 00:25:03 +0500

    Add more func

--- account/Account.go
+++ account/Account.go
@@ -13,8 +13,14 @@ type Account struct {
 	Age      uint
 }
 
+type PremiumAccount struct {
+	Account
+	Balance float64
+}
+
 func CreateAccount(id int, name string, lastName string, age uint) *Account {
 	return &Account{
+		Id:       id,
 		Name:     name,
 		LastName: lastName,
 		Age:      age,
@@ -29,4 +35,44 @@ func (account *Account) Info() {
 func (account *Account) GetId() int {
 	fmt.Print("Account Id: ")
 	return account.Id
+}
+
+func (account *Account) UpdateName(newName string) {
+	account.Name = newName
+}
+
+func CreatePremiumAccount(id int, name string, lastName string, age uint, balance float64) *PremiumAccount {
+	return &PremiumAccount{
+		Account: Account{
+			Id:       id,
+			Name:     name,
+			LastName: lastName,
+			Age:      age,
+		},
+		Balance: balance,
+	}
+}
+
+func (p *PremiumAccount) Deposit(amount float64) {
+	if amount > 0 {
+		p.Balance += amount
+		fmt.Println("Deposited: ", amount, "New Balance: ", p.Balance)
+	} else {
+		fmt.Println("Invalid deposit amount")
+	}
+}
+
+func (p *PremiumAccount) Withdraw(amount float64) {
+	if amount > 0 && amount <= p.Balance {
+		p.Balance -= amount
+		fmt.Println("Withdrawn: ", amount, "New Balance: ", p.Balance)
+	} else {
+		fmt.Println("Invalid Withdraw amount or insufficient balance")
+	}
+}
+
+func (p *PremiumAccount) Info() {
+	p.Account.Info()
+	fmt.Println("Balance: ", p.Balance)
+
 }
--- main.go
+++ main.go
@@ -9,8 +9,18 @@ func main() {
 	fmt.Println()
 	user1 := account.CreateAccount(1, "Valodya", "Vasilevich", 25)
 	user2 := account.CreateAccount(2, "Alesha", "Medvedev", 24)
+	user1.UpdateName("Vladimir")
 	accInterface := account.AccountInterface(user1)
 	fmt.Println(accInterface.GetId())
 	accInterface2 := account.AccountInterface(user2)
 	accInterface2.Info()
+
+	premAcc := account.CreatePremiumAccount(3, "Stas", "Blagov", 29, 25000.00)
+	premInterface := account.AccountInterface(premAcc)
+
+	fmt.Println(premInterface.GetId())
+	premInterface.Info()
+	premAcc.Deposit(5000)
+	premAcc.Withdraw(1000)
+	premInterface.Info()
 }
